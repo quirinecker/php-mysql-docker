a2df402d249494cc4da872ad3a968b036b365c8e
QuirinEcker <quirin.ecker@bajupa.com>
2020-04-23 10:25:50 +0200

    Initial commit

---
+++ removedocker.sh
@@ -0,0 +1,5 @@
+echo removing docker container
+docker container rm wdt_php
+docker container rm wdt_httpd
+docker container rm wdt_mysql
+docker container rm wdt_phpmyadmin
---
+++ stopdocker.sh
@@ -0,0 +1,5 @@
+echo stopping currently running web-docker-template docker
+docker stop wdt_php
+docker stop wdt_httpd
+docker stop wdt_mysql
+docker stop wdt_phpmyadmin
